17a3a467ad7783a109da4e8ccc4a9f11d98948e7
Serghei Iakovlev <serghei@phalconphp.com>
2018-10-17 10:59:33 +0300

    should work now

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -11,17 +11,21 @@ PROJECT_ROOT=$(readlink -enq "$(dirname $0)/../")
 
 shopt -s nullglob
 
-export NO_INTERACTION=1
-export REPORT_EXIT_STATUS=1
-export ZEND_DONT_UNLOAD_MODULES=1
-export USE_ZEND_ALLOC=0
-
 PHP_MAJOR="$(`phpenv which php` -r 'echo phpversion();' | cut -d '.' -f 1)"
 PHP_MINOR="$(`phpenv which php` -r 'echo phpversion();' | cut -d '.' -f 2)"
 
-# PHP 7.3 - 7.4 sill have memory leaks
-if [ "${PHP_MAJOR}.${PHP_MINOR}" = "7.4" ] || [ "${PHP_MAJOR}.${PHP_MINOR}" = "7.3" ]; then
-	NO_INTERACTION=1 make -C "${PROJECT_ROOT}" test
-else
-	NO_INTERACTION=1 make -C "${PROJECT_ROOT}" TESTS=-m test
-fi
+pushd "${PROJECT_ROOT}"
+
+	export NO_INTERACTION=1
+	export REPORT_EXIT_STATUS=1
+	export ZEND_DONT_UNLOAD_MODULES=1
+	export USE_ZEND_ALLOC=0
+
+	# PHP 7.3 - 7.4 sill have memory leaks
+	if [ "${PHP_MAJOR}.${PHP_MINOR}" = "7.4" ] || [ "${PHP_MAJOR}.${PHP_MINOR}" = "7.3" ]; then
+		make test
+	else
+		make TESTS=-m test
+	fi
+
+popd
