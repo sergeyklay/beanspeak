d6aec398b45ae0d058be5314781fde6cc55a1adf
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-12 01:22:33 +0200

    [WIP] Add free_obj, read_property and write_property handlers

--- php_beanspeak.c
+++ php_beanspeak.c
@@ -13,21 +13,130 @@
 
 #include <php.h>
 #include <php_ini.h>
+#include <Zend/zend_hash.h>
+#include <Zend/zend_object_handlers.h>
+#include <Zend/zend_operators.h>
 #include <ext/standard/info.h>
 
 #include "php_beanspeak.h"
 #include "beanspeak/client.h"
 #include "beanspeak/exception.h"
 
-/* {{{ INI_ENTRIES
- */
+/* {{{ class entries */
+PHP_BEANSPEAK_API zend_class_entry *beanspeak_client_ce_ptr;
+PHP_BEANSPEAK_API zend_class_entry *beanspeak_exceptioninterface_ce_ptr;
+PHP_BEANSPEAK_API zend_class_entry *beanspeak_invalidargumentexception_ce_ptr;
+/* }}} */
+
+zend_object_handlers beanspeak_object_handlers;
+
+static HashTable beanspeak_classes;
+
+/* {{{ beanspeak_classes_ctor */
+static void
+beanspeak_ce_storage_init()
+{
+	zend_hash_init(&beanspeak_classes, 0, NULL, NULL, 1);
+}
+/* }}} */
+
+/* {{{ beanspeak_classes_ctor */
+static void
+beanspeak_ce_storage_destroy()
+{
+	zend_hash_destroy(&beanspeak_classes);
+}
+/* }}} */
+
+/* {{{ prop handler tables */
+static HashTable beanspeak_client_prop_handlers;
+/* }}} */
+
+typedef int (*beanspeak_read_t)(beanspeak_object_t *obj, zval *retval);
+typedef int (*beanspeak_write_t)(beanspeak_object_t *obj, zval *newval);
+
+typedef struct _beanspeak_prop_handler {
+	beanspeak_read_t read_func;
+	beanspeak_write_t write_func;
+} beanspeak_prop_handler;
+
+/* {{{ beanspeak_get_object_handlers */
+static zend_object_handlers*
+beanspeak_get_object_handlers(void) {
+	return &beanspeak_object_handlers;
+}
+/* }}} */
+
+/* {{{ beanspeak_objects_free_storage */
+static void
+beanspeak_objects_free_storage(zend_object *object)
+{
+	beanspeak_object_t *intern = beanspeak_obj_from_zo(object);
+
+	zend_object_std_dtor(&intern->zo);
+}
+
+/* {{{ beanspeak_read_property */
+static zval*
+beanspeak_read_property(zval *object, zval *member, int type, void **cache_slot, zval *rv)
+{
+	beanspeak_object_t *obj = Z_BEANSPEAK_OBJ_P(object);
+	zval *retval;
+	beanspeak_prop_handler *hnd = NULL;
+
+	BEANSPEAK_PROPERTY_HANDLER_PROLOG;
+
+	if (obj->prop_handler != NULL) {
+		hnd = zend_hash_find_ptr(obj->prop_handler, Z_STR_P(member));
+	}
+
+	if (hnd != NULL) {
+		int ret = hnd->read_func(obj, rv);
+		if (ret == SUCCESS) {
+			retval = rv;
+		} else {
+			retval = &EG(uninitialized_zval);
+		}
+	} else {
+		retval = zend_std_read_property(object, member, type, cache_slot, rv);
+	}
+
+	BEANSPEAK_PROPERTY_HANDLER_EPILOG;
+
+	return retval;
+}
+/* }}} */
+
+/* {{{ beanspeak_write_property */
+static void
+beanspeak_write_property(zval *object, zval *member, zval *value, void **cache_slot)
+{
+	beanspeak_object_t *obj = Z_BEANSPEAK_OBJ_P(object);
+	beanspeak_prop_handler *hnd = NULL;
+
+	BEANSPEAK_PROPERTY_HANDLER_PROLOG;
+
+	if (obj->prop_handler != NULL) {
+		hnd = zend_hash_find_ptr(obj->prop_handler, Z_STR_P(member));
+	}
+
+	if (hnd != NULL) {
+		hnd->write_func(obj, value);
+	} else {
+		zend_std_write_property(object, member, value, cache_slot);
+	}
+
+	BEANSPEAK_PROPERTY_HANDLER_EPILOG;
+}
+/* }}} */
+
+/* {{{ INI_ENTRIES */
 PHP_INI_BEGIN()
 
 PHP_INI_END()
 /* }}} */
 
-/* {{{ PHP_RINIT_FUNCTION
- */
+/* {{{ PHP_RINIT_FUNCTION */
 PHP_RINIT_FUNCTION(beanspeak)
 {
 #if defined(ZTS) && defined(COMPILE_DL_BEANSPEAK)
@@ -44,6 +153,15 @@ static PHP_MINIT_FUNCTION(beanspeak)
 {
 	REGISTER_INI_ENTRIES();
 
+	memcpy(&beanspeak_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+
+	beanspeak_object_handlers.offset = (int) XtOffsetOf(beanspeak_object_t, zo);
+	beanspeak_object_handlers.free_obj = beanspeak_objects_free_storage;
+	beanspeak_object_handlers.read_property = beanspeak_read_property;
+	beanspeak_object_handlers.write_property = beanspeak_write_property;
+
+	beanspeak_ce_storage_init();
+
 	BEANSPEAK_INIT(Beanspeak_Client);
 	BEANSPEAK_INIT(Beanspeak_ExceptionInterface);
 	BEANSPEAK_INIT(Beanspeak_InvalidArgumentException);
@@ -57,6 +175,8 @@ static PHP_MINIT_FUNCTION(beanspeak)
 static PHP_MSHUTDOWN_FUNCTION(beanspeak)
 {
 	UNREGISTER_INI_ENTRIES();
+
+	beanspeak_ce_storage_destroy();
 
 	return SUCCESS;
 }
