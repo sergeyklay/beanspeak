c98bf68da75003b48eb8f7bd46f58297540c1d2c
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-12 11:16:19 +0200

    [WIP] Correct test runner

--- beanspeak.mk
+++ beanspeak.mk
@@ -6,15 +6,17 @@ TESTSDIR = $(top_srcdir)/tests
 clean-dirs:
 	rm -rf $(top_srcdir)/include
 
-.PHONY: after-failure
-after-failure:
+.PHONY: investigation
+investigation:
 	@for i in `echo "$(wildcard $(TESTSDIR)/*.out)"`; do \
 		echo "-- START $$i"; \
 		cat $$i; \
+		echo \
 		echo "-- END"; \
 	done;
 	@for i in `echo "$(wildcard $(TESTSDIR)/*.mem)"`; do \
 		echo "-- START $$i"; \
 		cat $$i; \
+		echo \
 		echo "-- END"; \
 	done;
